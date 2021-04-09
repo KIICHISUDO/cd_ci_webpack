5a6cf54f99c83c9ee42fbf087a6e474bf664bc5d
KIICHISUDO <kiichisudo999@hotmail.com>
2021-04-09 15:42:18 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,20 @@
+const CopyPlugin = require("copy-webpack-plugin");
+
 module.exports = {
     mode: "development",
     entry: "./src/index.js",
     output: {
         path: `${__dirname}/public`,
         filename: "main.js"
-    }
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: "./index.html",
+                    to: "./public/"
+                }
+            ]
+        })
+    ]
 };
